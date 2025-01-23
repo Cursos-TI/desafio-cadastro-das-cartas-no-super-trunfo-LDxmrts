d58415ba80202ebb08092ef8ca9d7a55a850498c
LDxmrts <87765239+LDxmrts@users.noreply.github.com>
2025-01-23 22:53:48 +0000

    commit

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -15,6 +15,9 @@ int main() {
     float area;
     float pib;
     int pontos_turisticos;
+    float populacaoporarea;
+    float pibporpopulacao;
+
 
     //Solicitação de informações do usuário
     printf("Digite o código da cidade iniciando com uma letra entre A e H e terminando com um número entre 1 e 4\n");
@@ -28,12 +31,20 @@ int main() {
     printf ("Digite o número de pontos turísticos\n");
     scanf ("%d", &pontos_turisticos);
 
+    //Cálculos necessários
+
+    populacaoporarea = populacao / area;
+    pibporpopulacao =  pib / populacao;
+
     //Exibição dos valores
     printf ("O código da cidade é %s\n", codigo);
     printf ("A população da cidade é %d habitantes\n", cidade_numero);
     printf ("A área da cidade é %.2f km²\n", area);
     printf ("O PIB é %.2f milhões\n", pib);
     printf ("O número de pontos turísticos é %d\n", pontos_turisticos);
+    printf ("A densidade demografica da cidade é: %.2f pessoas/km2\n", populacaoporarea);
+    printf ("O PIB per capita da cidade é: %.2f reais\n", pibporpopulacao);
+
 
     return 0;
 }
